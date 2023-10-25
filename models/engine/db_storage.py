c82b6d48d2caa9cd7d4ea9e7a1973051fa3def6c
Chekwasy <richardchekwas@gmail.com>
2023-10-25 12:44:09 +0100

    uu

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,20 +4,14 @@ Contains the class DBStorage
 """
 
 import models
-from models.amenity import Amenity
 from models.base_model import BaseModel, Base
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models.user import Employee
 from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"Employee": Employee}
 
 
 class DBStorage:
@@ -27,17 +21,17 @@ class DBStorage:
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
+        TP_MYSQL_USER = getenv('TP_MYSQL_USER')
+        TP_MYSQL_PWD = getenv('TP_MYSQL_PWD')
+        TP_MYSQL_HOST = getenv('TP_MYSQL_HOST')
+        TP_MYSQL_DB = getenv('TP_MYSQL_DB')
+        TP_ENV = getenv('TP_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(HBNB_MYSQL_USER,
-                                             HBNB_MYSQL_PWD,
-                                             HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
-        if HBNB_ENV == "test":
+                                      format(TP_MYSQL_USER,
+                                             TP_MYSQL_PWD,
+                                             TP_MYSQL_HOST,
+                                             TP_MYSQL_DB))
+        if TP_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
