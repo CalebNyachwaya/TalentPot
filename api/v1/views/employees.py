c5420234b73176618db6e60bc75a9696219bc7be
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2023-10-27 12:43:57 +0100

    aa

--- api/v1/views/employees.py
+++ api/v1/views/employees.py
@@ -27,12 +27,12 @@ def get_employees(company):
         dict_employees.clear()
     return jsonify(list_employees)
 
-
+"""
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/user/get_user.yml', methods=['GET'])
 def get_user(user_id):
-    """ Retrieves an user """
-    user = storage.get(User, user_id)
+   """ """ Retrieves an user """
+    """user = storage.get(User, user_id)
     if not user:
         abort(404)
 
@@ -44,10 +44,10 @@ def get_user(user_id):
 @swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
 def delete_user(user_id):
     """
-    Deletes a user Object
+    """Deletes a user Object
     """
 
-    user = storage.get(User, user_id)
+    """user = storage.get(User, user_id)
 
     if not user:
         abort(404)
@@ -62,9 +62,9 @@ def delete_user(user_id):
 @swag_from('documentation/user/post_user.yml', methods=['POST'])
 def post_user():
     """
-    Creates a user
+    """Creates a user
     """
-    if not request.get_json():
+    """if not request.get_json():
         abort(400, description="Not a JSON")
 
     if 'email' not in request.get_json():
@@ -82,9 +82,9 @@ def post_user():
 @swag_from('documentation/user/put_user.yml', methods=['PUT'])
 def put_user(user_id):
     """
-    Updates a user
+    """Updates a user
     """
-    user = storage.get(User, user_id)
+    """user = storage.get(User, user_id)
 
     if not user:
         abort(404)
@@ -100,3 +100,4 @@ def put_user(user_id):
             setattr(user, key, value)
     storage.save()
     return make_response(jsonify(user.to_dict()), 200)
+"""
