a8a56634184eafaa65888d4f1c97bc8f5161e91d
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-11-13 14:16:24 +0000

    Update forms

--- web_flask/static/scripts/all_script.js
+++ web_flask/static/scripts/all_script.js
@@ -1,6 +1,6 @@
 (document).ready(function () {
     $("#asub").on("submit", function (event) {
-	const api = "http://" + "calebcodes.tech/employees/";
+	const api = "http://" + "calebcodes.tech/api/v1/employees/";
 	const comp = stringify($("#company".val()));
 	$.ajax({
 	    url: api + comp,
@@ -26,50 +26,5 @@
 		alert("Employee added sucessfully");
 	    })
 	event.preventDefault();
-    })
-    $("#msub").on("submit", function (event) {
-	const api = "http://" + "calebcodes.tech/employees/";
-	const comp = stringify($("#company".val()));
-	$.ajax({
-	    url: api + comp,
-	    type: "POST",
-	    data: JSON.stringify({
-		company: $("#company").val(),
-		email: $("#email").val(),
-		password: $("#password").val(),
-		DOB: $("#DOB").val(),
-		phone: $("#phone").val(),
-		city: $("#city").val(),
-		address: $("#address").val(),
-		country: $("#country").val(),
-		dept: $("#dept").val(),
-		position: $("#position").val()
-	    }),
-	    contentType: "application/json",
-	    dataType: "json",
 	})
-	    .done(function (data) {
-		alert("Employee modified sucessfully");
-	    })
-	event.preventDefault();
     })
-    $("#dsub").on("submit", function (event) {
-	const api = "http://" + "calebcodes.tech/employees/";
-	const comp = stringify($("#company".val()));
-	$.ajax({
-	    url: api + comp,
-	    type: "POST",
-	    data: JSON.stringify({
-		company: $("#company").val(),
-		email: $("#email").val(),
-		password: $("#password").val()
-	    }),
-	    contentType: "application/json",
-	    dataType: "json",
-	})
-	    .done(function (data) {
-		alert("Employee deleted sucessfully");
-	    })
-	event.preventDefault();
-    })
-});
