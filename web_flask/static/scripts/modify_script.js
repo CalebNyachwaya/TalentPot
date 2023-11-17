d4345115deaaaff52d1ce6a18bcba78a4276dd51
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2023-11-17 14:24:54 +0100

    aa

--- web_flask/static/scripts/modify_script.js
+++ web_flask/static/scripts/modify_script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#Save").click(function () {
+    $("INPUT#Save").click(function () {
 	const api = "http://" + "calebcodes.tech/api/v1/modify/employees/";
 	const comp = $("#company".val());
 	const eml = $("#email".val());
@@ -7,16 +7,16 @@ $(document).ready(function () {
 	    url: api + comp + "/" + eml,
 	    type: "PUT",
 	    data: JSON.stringify({
-		company: $("#company").val(),
-		email: $("#email").val(),
-		password: $("#password").val(),
-		DOB: $("#DOB").val(),
-		phone: $("#phone").val(),
-		city: $("#city").val(),
-		address: $("#address").val(),
-		country: $("#country").val(),
-		dept: $("#dept").val(),
-		position: $("#position").val()
+		company: $("INPUT#company").val(),
+		email: $("INPUT#email").val(),
+		password: $("INPUT#password").val(),
+		DOB: $("INPUT#DOB").val(),
+		phone: $("INPUT#phone").val(),
+		city: $("INPUT#city").val(),
+		address: $("INPUT#address").val(),
+		country: $("INPUT#country").val(),
+		dept: $("INPUT#dept").val(),
+		position: $("INPUT#position").val()
 	    }),
 	    contentType: "application/json",
 	    dataType: "json"
