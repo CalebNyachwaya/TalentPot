752f0ce0ecdf8cd55e5241b5b41005ff639aba8d
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-03-18 00:08:58 +0100

    update

--- api/v2/auth/auth.py
+++ api/v2/auth/auth.py
@@ -162,7 +162,7 @@ session in db to none"""
             dept = usr_dt.get("dept")
             position = usr_dt.get("position")
             DOB = usr_dt.get("DOB")
-	    company = usr_dt.get("company")
+            company = usr_dt.get("company")
             address = usr_dt.get("address")
             city = usr_dt.get("city")
             country = usr_dt.get("country")
